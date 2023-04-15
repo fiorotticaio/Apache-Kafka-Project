2dcb90521d72d234c423f4bb27a500e34a5db93d
fiorotticaio <caiofiorotti@gmail.com>
2023-04-15 11:33:43 -0300

    :poop: testing get data in api

--- src/main/java/kafka/Consumer.java
+++ src/main/java/kafka/Consumer.java
@@ -6,16 +6,20 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.time.Duration;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.time.Duration; 
 
 
 public class Consumer {
   public static void main(String[] args) {
     String BootstrapServers = "localhost:9092";
-    String Topic = "testTopic";
+    String topic = "sbux_stock";
 
     Logger logger = LoggerFactory.getLogger(Consumer.class.getName());
 
@@ -23,21 +27,28 @@ public class Consumer {
     prop.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BootstrapServers);
     prop.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
     prop.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+    
+    // Configurações de grupos de consumidores
     prop.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group-1");
-    prop.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+    prop.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+    prop.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+    prop.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1000");
 
     KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(prop);
 
-    consumer.subscribe(Arrays.asList(Topic));
+    // Assina o tópico "sbux_stock"
+    consumer.subscribe(Arrays.asList(topic));
 
     while (true) {
+      // TODO: mudar o consumidor pra ele receber JSON
       ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
       for (ConsumerRecord<String, String> record : records) {
-        logger.info("Key: " + record.key() + ", Value: " + record.value());
-        logger.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+        logger.info("Key: " + record.key() + ", Value:");
+        logger.info("\n");
+        logger.info(record.value());
+        logger.info("\n\n");
       }
     }
-
-    
   }
 }
--- src/main/java/kafka/Producer.java
+++ src/main/java/kafka/Producer.java
@@ -1,27 +1,71 @@
 package kafka;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import java.util.Properties;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class Producer {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException, IOException {
     String BootstrapServers = "localhost:9092";
-    String Topic = "testTopic";
+    String topic = "sbux_stock";
+    Logger logger = LoggerFactory.getLogger(Producer.class);
 
+    /* Setting procudor properties */
     Properties prop = new Properties();
     prop.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BootstrapServers);
     prop.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
     prop.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
-    KafkaProducer<String, String> producer = new KafkaProducer<String, String>(prop);
+    KafkaProducer<String, String> producer = new KafkaProducer<String, String>(prop); // create the producer
 
-    ProducerRecord<String, String> record = new ProducerRecord<String, String>(Topic, "Hello, Kafka!");
+    /* Connection with Alpha Vantage API */
+    String function = "TIME_SERIES_INTRADAY";
+    String apiKey = "0WCXGJ9X5SRNOH5M";
+    String symbol = "SBUX";
+    String interval = "1min";
+    String url = String.format("https://www.alphavantage.co/query?function=%s&interval=%s&symbol=%s&apikey=%s", function, interval, symbol, apiKey);
 
-    producer.send(record);
-    producer.flush(); // Atualiza o producer
+    HttpClient client = HttpClientBuilder.create().build();
+
+    while (true) {
+      HttpGet request = new HttpGet(url);
+      URL apiUrl = new URL(url);
+      HttpURLConnection conn = (HttpURLConnection) apiUrl.openConnection();
+      conn.setRequestMethod("GET");
+      JSONObject data = null;
+
+      try {
+        HttpResponse response = client.execute(request);
+        String json = EntityUtils.toString(response.getEntity());
+        data = new JSONObject(json);
+      } catch (Exception ex) {
+        ex.printStackTrace();
+      }
+
+      System.out.println("JSON GERADO: " + data);
+
+      // TODO: Mudar o produtor para enviar o JSON inteiro
+      // ProducerRecord<String, String> record = new ProducerRecord<>(topic, data);
+      // producer.send(record);
+      Thread.sleep(60000); // espera 1min segundos antes de buscar novos dados
+    }
   }
 }
