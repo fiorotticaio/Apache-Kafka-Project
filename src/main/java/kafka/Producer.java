06871b14f860be097daedf0bb16b94a2685269bd
fiorotticaio <caiofiorotti@gmail.com>
2023-04-21 19:43:09 -0300

    :recycle: taking off useless code

--- src/main/java/kafka/Producer.java
+++ src/main/java/kafka/Producer.java
@@ -1,12 +1,10 @@
 package kafka;
 
-
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
-import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -18,15 +16,12 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 
 public class Producer {
   public static void main(String[] args) throws InterruptedException, IOException {
     String BootstrapServers = "localhost:9092";
     String topic = "sbux_stock";
-    Logger logger = LoggerFactory.getLogger(Producer.class);
 
     /* Setting procudor properties */
     Properties prop = new Properties();
@@ -44,20 +39,17 @@ public class Producer {
     String url = String.format("https://www.alphavantage.co/query?function=%s&interval=%s&symbol=%s&apikey=%s", function, interval, symbol, apiKey);
 
     HttpClient client = HttpClientBuilder.create().build();
-    int i=0;
+    int i = 0;
     while (true) {
       i+=1;
       HttpGet request = new HttpGet(url);
       URL apiUrl = new URL(url);
       HttpURLConnection conn = (HttpURLConnection) apiUrl.openConnection();
       conn.setRequestMethod("GET");
-      // JSONObject value = null;
 
       try {
         HttpResponse response = client.execute(request);
         String responseObject = EntityUtils.toString(response.getEntity());
-        // value = new JSONObject(json);
-        // System.out.println("JSON GERADO: " + value);
         byte[] value = responseObject.getBytes(StandardCharsets.UTF_8);
         ProducerRecord<String, byte[]> record = new ProducerRecord<String, byte[]>(topic, "id_"+i, value);
         producer.send(record);
@@ -65,10 +57,7 @@ public class Producer {
         ex.printStackTrace();
       }
 
-
-      
-      
-      Thread.sleep(20000); // espera 1min segundos antes de buscar novos dados
+      Thread.sleep(20000); // wait 1min before get new data
     }
   }
 }
