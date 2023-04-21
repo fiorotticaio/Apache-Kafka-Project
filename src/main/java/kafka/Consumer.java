f79685ad024f9fb501dd3f993ab4d6fba58a73c9
fiorotticaio <caiofiorotti@gmail.com>
2023-04-21 19:43:45 -0300

    :sparkles: testing calculate avarege data

--- src/main/java/kafka/Consumer.java
+++ src/main/java/kafka/Consumer.java
@@ -29,7 +29,7 @@ public class Consumer {
     prop.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
     prop.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class.getName());
     
-    // Configurações de grupos de consumidores
+    /* Consumer group settings */
     prop.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group-1");
     prop.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
     prop.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
@@ -37,8 +37,18 @@ public class Consumer {
 
     KafkaConsumer<String, byte[]> consumer = new KafkaConsumer<>(prop);
 
-    // Assina o tópico "sbux_stock"
+    /* Subscribe in topic "sbux_stock" */
     consumer.subscribe(Arrays.asList(topic));
+
+    /* Our historical average */
+    double openAverage = 0.0, openAverageAux = 0.0;
+    double highAverage = 0.0, highAverageAux = 0.0;
+    double lowAverage = 0.0, lowAverageAux = 0.0;
+    double closeAverage = 0.0, closeAverageAux = 0.0;
+    double volumeAverage = 0.0, volumeAverageAux = 0.0;
+    int countSales = 0, recordCounts = 0;
+
+    int maior = 0, menor = 0, igual = 0;
 
     while (true) {
       ConsumerRecords<String, byte[]> records = consumer.poll(Duration.ofMillis(1000));
@@ -48,15 +58,79 @@ public class Consumer {
         byte[] valueBytes = record.value();
         JSONObject data = new JSONObject(new String(valueBytes, StandardCharsets.UTF_8));
 
-        /* Testing to read specific atributes of the data */
-        JSONObject allSales = data.getJSONObject("Time Series (1min)");
-        JSONObject specificSale = allSales.getJSONObject("2023-04-14 15:17:00");
-        String high = specificSale.getString("2. high");
-        logger.info("High: " + high);
+        System.out.println(data);
 
-        // logger.info(data.toString());
-        // logger.info("\n\n");
+        JSONObject timeSeries = data.getJSONObject("Time Series (1min)"); // Object with all seals
+        for (String key : timeSeries.keySet()) { // Go through all sales
+          JSONObject timeSeriesObj = timeSeries.getJSONObject(key);
+          Double open = Double.parseDouble(timeSeriesObj.getString("1. open")); // Get the open field
+          Double high = Double.parseDouble(timeSeriesObj.getString("2. high")); // Get the high field
+          Double low = Double.parseDouble(timeSeriesObj.getString("3. low")); // Get the low field
+          Double close = Double.parseDouble(timeSeriesObj.getString("4. close")); // Get the close field
+          Double volume = Double.parseDouble(timeSeriesObj.getString("5. volume")); // Get the volume field
+
+          if (recordCounts != 0) { // If it is not the first time, we have to compare if increase or decrease
+            if (high > highAverage) maior++;
+            else if (high < highAverage) menor++;
+            else igual++;
+          } 
+
+          openAverageAux += open;
+          highAverageAux += high;
+          lowAverageAux += low;
+          closeAverageAux += close;
+          volumeAverageAux += volume;
+
+          countSales += 1;
+        }
+
+        /* Calculating the average of the last "countSales" sales */
+        openAverageAux /= countSales;
+        highAverageAux /= countSales;
+        lowAverageAux /= countSales;
+        closeAverageAux /= countSales;
+        volumeAverageAux /= countSales;
+
+        /* Updating the historical average */
+        if (recordCounts == 0) { // If it is the first time, we have to set the historical average
+          openAverage = openAverageAux;
+          highAverage = highAverageAux;
+          lowAverage = lowAverageAux;
+          closeAverage = closeAverageAux;
+          volumeAverage = volumeAverageAux;
+        } else {
+          openAverage = (openAverage + openAverageAux) / 2;
+          highAverage = (highAverage + highAverageAux) / 2;
+          lowAverage = (lowAverage + lowAverageAux) / 2;
+          closeAverage = (closeAverage + closeAverageAux) / 2;
+          volumeAverage = (volumeAverage + volumeAverageAux) / 2;
+
+          System.out.println("Maior: " + maior);
+          System.out.println("Menor: " + menor);
+          System.out.println("Igual: " + igual);
+        }
+
+        /* Resetting aux variables */
+        openAverageAux = 0.0;
+        highAverageAux = 0.0;
+        lowAverageAux = 0.0;
+        closeAverageAux = 0.0;
+        volumeAverageAux = 0.0;
+        countSales = 0;
+
+        recordCounts++;
+
+        maior = 0;
+        menor = 0;
+        igual = 0;
       }
+
+      // System.out.println("Open average: " + openAverage);
+      // System.out.println("High average: " + highAverage);
+      // System.out.println("Low average: " + lowAverage);
+      // System.out.println("Close average: " + closeAverage);
+      // System.out.println("Volume average: " + volumeAverage);
+      // System.out.print("\n");
     }
   }
 }
